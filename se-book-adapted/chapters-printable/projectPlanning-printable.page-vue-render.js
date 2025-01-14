f48642354a233277f346461a1f341193fd53c702
damithc <1673303+damithc@users.noreply.github.com>
2025-01-15 00:07:12 +0800

    Site Update. [skip ci]

--- se-book-adapted/chapters-printable/projectPlanning-printable.page-vue-render.js
+++ se-book-adapted/chapters-printable/projectPlanning-printable.page-vue-render.js
@@ -38,6 +38,6 @@ with(this){return _c('p',[_c('strong',[_v("Issue trackers (sometimes called bug 
 with(this){return _c('p',[_c('strong',[_v("A "),_c('em',[_v("Gantt chart")]),_v(" is a 2-D bar-chart, drawn as "),_c('em',[_v("time vs tasks")])]),_v(" (represented by horizontal bars).")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Tue, 14 Jan 2025, 19:02:49 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Wed, 15 Jan 2025, 0:05:50 GMT+8]")])])])}
 }];
   
