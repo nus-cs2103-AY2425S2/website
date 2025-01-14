9c3b64dd12f5122823850246afa369dad0f27566
damithc <1673303+damithc@users.noreply.github.com>
2025-01-14 19:04:07 +0800

    Site Update. [skip ci]

--- schedule/week1/admin.page-vue-render.js
+++ schedule/week1/admin.page-vue-render.js
@@ -56,6 +56,6 @@ with(this){return _c('ul',[_c('li',[_v("Follow the tool "),_c('span',{staticClas
 with(this){return _c('div',[_c('ul',[_c('li',[_v("If you have time to spare while waiting for the semester to start, and would like to get a headstart on learning Git and GitHub (used a lot in this course), you are welcome to follow the "),_c('a',{attrs:{"href":"/website/git-trail/index.html"}},[_v("Git Learning Trail")]),_v("."),_c('br'),_v("\nDoing so is totally optional, as the same content will be covered during the subsequent weeks as well.")])])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Tue, 14 Jan 2025, 18:35:42 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Tue, 14 Jan 2025, 19:02:49 GMT+8]")])])])}
 }];
   
