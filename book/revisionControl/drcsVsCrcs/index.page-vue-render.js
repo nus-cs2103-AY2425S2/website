a0a7b39da18259fe52ec9000ad23e52d2ab00dd9
damithc <1673303+damithc@users.noreply.github.com>
2025-01-18 22:55:22 +0800

    Site Update. [skip ci]

--- book/revisionControl/drcsVsCrcs/index.page-vue-render.js
+++ book/revisionControl/drcsVsCrcs/index.page-vue-render.js
@@ -26,6 +26,6 @@ with(this){return _c('p',[_c('strong',[_v("Centralized RCS (CRCS for short) uses
 with(this){return _c('p',[_c('strong',[_v("Distributed RCS (DRCS for short, also known as Decentralized RCS) allows multiple remote/local repos")]),_v(" working together. The workflow can vary from team to team.  For example, every team member can have his/her own remote repository in addition to their own local repository, as shown in the diagram below. Git and Mercurial are some prominent RCS tools that support the distributed approach.")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Sat, 18 Jan 2025, 19:02:12 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Sat, 18 Jan 2025, 22:53:58 GMT+8]")])])])}
 }];
   
--- schedule/week12/topics.page-vue-render.js
+++ schedule/week12/topics.page-vue-render.js
@@ -17,6 +17,6 @@ with(this){return _c('p',[_c('strong',[_v("No more topics left!")]),_c('br')])}
 with(this){return _c('small',[_c('span',{staticClass:"dimmed"},[_v("[image source: esmemes.com]")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Sat, 18 Jan 2025, 19:02:12 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Sat, 18 Jan 2025, 22:53:58 GMT+8]")])])])}
 }];
   
