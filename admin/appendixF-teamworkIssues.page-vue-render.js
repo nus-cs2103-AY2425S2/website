0134901b4b540f965f9dc948f45945188c9b28ad
damithc <1673303+damithc@users.noreply.github.com>
2025-03-05 23:06:06 +0800

    Site Update. [skip ci]

--- admin/appendixF-teamworkIssues.page-vue-render.js
+++ admin/appendixF-teamworkIssues.page-vue-render.js
@@ -23,6 +23,6 @@ with(this){return _c('p',[_c('strong',[_v("Now, some common problems and guidanc
 with(this){return _c('p',[_c('span',{staticClass:"float-start algolia-no-index"},[_c('a',{staticClass:"badge bg-light text-dark",attrs:{"href":"/website/admin/appendixE-gitHub.html"}},[_c('span',[_c('span',{staticClass:"far fa-arrow-alt-circle-left",attrs:{"aria-hidden":"true"}}),_v(" Apdx E: Using GitHub")])])])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Wed, 5 Mar 2025, 20:48:44 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Wed, 5 Mar 2025, 23:04:31 GMT+8]")])])])}
 }];
   
