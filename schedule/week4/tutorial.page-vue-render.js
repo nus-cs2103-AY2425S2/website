aad908301c15e6163122977de76a2c4f3de3239b
damithc <1673303+damithc@users.noreply.github.com>
2025-03-14 15:39:35 +0800

    Site Update. [skip ci]

--- schedule/week4/tutorial.page-vue-render.js
+++ schedule/week4/tutorial.page-vue-render.js
@@ -14,6 +14,6 @@ with(this){return _c('h1',{attrs:{"id":"week-4-mon-feb-3rd-tutorial"}},[_v("Week
 with(this){return _c('ul',[_c('li',[_v("Do the first of the two exercise given below, as directed by the tutor. If there is time left, do the second one as well.")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Fri, 14 Mar 2025, 2:18:23 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Fri, 14 Mar 2025, 15:37:57 GMT+8]")])])])}
 }];
   
