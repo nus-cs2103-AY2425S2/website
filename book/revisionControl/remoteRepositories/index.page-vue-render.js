08161b04d3a29a5ff76b0d84719125955e6e4c53
damithc <1673303+damithc@users.noreply.github.com>
2025-04-04 14:34:10 +0800

    Site Update. [skip ci]

--- book/revisionControl/remoteRepositories/index.page-vue-render.js
+++ book/revisionControl/remoteRepositories/index.page-vue-render.js
@@ -41,6 +41,6 @@ with(this){return _c('div',{attrs:{"id":"section-forking"}},[_c('p',[_c('strong'
 with(this){return _c('strong',[_v("A "),_c('mark',[_c('em',[_v("pull request")])]),_v(" (PR for short) is a mechanism for contributing code to a remote repo")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.6.0")]),_v(", generated on Fri, 4 Apr 2025, 12:27:00 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.6.0")]),_v(", generated on Fri, 4 Apr 2025, 14:32:37 GMT+8]")])])])}
 }];
   
--- schedule/week1/admin.page-vue-render.js
+++ schedule/week1/admin.page-vue-render.js
@@ -53,6 +53,6 @@ with(this){return _c('ul',[_c('li',[_v("Follow the tool "),_c('span',{staticClas
 with(this){return _c('div',[_c('ul',[_c('li',[_v("If you have time to spare while waiting for the semester to start, and would like to get a headstart on learning Git and GitHub (used a lot in this course), you are welcome to follow the "),_c('a',{attrs:{"href":"/website/git-trail/index.html"}},[_v("Git Learning Trail")]),_v("."),_c('br'),_v("\nDoing so is totally optional, as the same content will be covered during the subsequent weeks as well.")])])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.6.0")]),_v(", generated on Fri, 4 Apr 2025, 12:27:00 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.6.0")]),_v(", generated on Fri, 4 Apr 2025, 14:32:37 GMT+8]")])])])}
 }];
   
