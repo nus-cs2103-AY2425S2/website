a6208e8f259499f508d2d17054b89b8c305806e8
damithc <1673303+damithc@users.noreply.github.com>
2025-04-06 21:29:24 +0800

    Site Update. [skip ci]

--- schedule/week13/project.page-vue-render.js
+++ schedule/week13/project.page-vue-render.js
@@ -29,6 +29,6 @@ with(this){return _c('span',[_c('span',[_c('span',{staticClass:"fas fa-users",at
 with(this){return _c('div',[_c('p',[_v("This is the time to attend to post-release tasks (as "),_c('code',{pre:true,attrs:{"class":"line-numbers hljs inline no-lang"}},[_v("v1.6")]),_v(" was considered a public release). In the context of our course, this translates to attending to the remaining phases of the PE.")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.6.0")]),_v(", generated on Sun, 6 Apr 2025, 17:54:19 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.6.0")]),_v(", generated on Sun, 6 Apr 2025, 21:27:48 GMT+8]")])])])}
 }];
   
